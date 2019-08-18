82f555c5c9ff2f5ab1198b175ee90352ceb0bea9
龚均强 <1208754526@qq.com>
2019-08-18 16:20:37 +0800

    资源文件添加

--- SMDB/SMDB/FrmMain.Designer.cs
+++ SMDB/SMDB/FrmMain.Designer.cs
@@ -28,12 +28,292 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.系统ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmiModifyPwd = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.tmiClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.学员管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAddStudent = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_Import = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiManageStudent = new System.Windows.Forms.ToolStripMenuItem();
+            this.成绩管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiQueryAndAnalysis = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiQuery = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_AttendanceQuery = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_Card = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator7 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmi_AQuery = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助HToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmi_linkxkt = new System.Windows.Forms.ToolStripMenuItem();
+            this.txmi_update = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator8 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmi_about = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblVersion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCurrentUser = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.系统ToolStripMenuItem,
+            this.学员管理ToolStripMenuItem,
+            this.成绩管理ToolStripMenuItem,
+            this.tsmi_AttendanceQuery,
+            this.帮助HToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 25);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 系统ToolStripMenuItem
+            // 
+            this.系统ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tmiModifyPwd,
+            this.toolStripSeparator3,
+            this.tmiClose});
+            this.系统ToolStripMenuItem.Name = "系统ToolStripMenuItem";
+            this.系统ToolStripMenuItem.Size = new System.Drawing.Size(59, 21);
+            this.系统ToolStripMenuItem.Text = "系统(&S)";
+            // 
+            // tmiModifyPwd
+            // 
+            this.tmiModifyPwd.Image = ((System.Drawing.Image)(resources.GetObject("tmiModifyPwd.Image")));
+            this.tmiModifyPwd.Name = "tmiModifyPwd";
+            this.tmiModifyPwd.Size = new System.Drawing.Size(180, 22);
+            this.tmiModifyPwd.Text = "密码修改(&C)";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tmiClose
+            // 
+            this.tmiClose.Image = ((System.Drawing.Image)(resources.GetObject("tmiClose.Image")));
+            this.tmiClose.Name = "tmiClose";
+            this.tmiClose.Size = new System.Drawing.Size(180, 22);
+            this.tmiClose.Text = "退出(&E)";
+            // 
+            // 学员管理ToolStripMenuItem
+            // 
+            this.学员管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAddStudent,
+            this.tsmi_Import,
+            this.toolStripSeparator2,
+            this.tsmiManageStudent});
+            this.学员管理ToolStripMenuItem.Name = "学员管理ToolStripMenuItem";
+            this.学员管理ToolStripMenuItem.Size = new System.Drawing.Size(88, 21);
+            this.学员管理ToolStripMenuItem.Text = "学员管理(&M)";
+            // 
+            // tsmiAddStudent
+            // 
+            this.tsmiAddStudent.Image = ((System.Drawing.Image)(resources.GetObject("tsmiAddStudent.Image")));
+            this.tsmiAddStudent.Name = "tsmiAddStudent";
+            this.tsmiAddStudent.Size = new System.Drawing.Size(180, 22);
+            this.tsmiAddStudent.Text = "添加学员(&A)";
+            // 
+            // tsmi_Import
+            // 
+            this.tsmi_Import.Image = ((System.Drawing.Image)(resources.GetObject("tsmi_Import.Image")));
+            this.tsmi_Import.Name = "tsmi_Import";
+            this.tsmi_Import.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_Import.Text = "批量导入学员";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tsmiManageStudent
+            // 
+            this.tsmiManageStudent.Image = ((System.Drawing.Image)(resources.GetObject("tsmiManageStudent.Image")));
+            this.tsmiManageStudent.Name = "tsmiManageStudent";
+            this.tsmiManageStudent.Size = new System.Drawing.Size(180, 22);
+            this.tsmiManageStudent.Text = "学员信息管理(&Q)";
+            // 
+            // 成绩管理ToolStripMenuItem
+            // 
+            this.成绩管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiQueryAndAnalysis,
+            this.toolStripSeparator1,
+            this.tsmiQuery});
+            this.成绩管理ToolStripMenuItem.Name = "成绩管理ToolStripMenuItem";
+            this.成绩管理ToolStripMenuItem.Size = new System.Drawing.Size(81, 21);
+            this.成绩管理ToolStripMenuItem.Text = "成绩管理(&J)";
+            // 
+            // tsmiQueryAndAnalysis
+            // 
+            this.tsmiQueryAndAnalysis.Image = ((System.Drawing.Image)(resources.GetObject("tsmiQueryAndAnalysis.Image")));
+            this.tsmiQueryAndAnalysis.Name = "tsmiQueryAndAnalysis";
+            this.tsmiQueryAndAnalysis.Size = new System.Drawing.Size(178, 22);
+            this.tsmiQueryAndAnalysis.Text = "成绩查询与分析(&Q)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(175, 6);
+            // 
+            // tsmiQuery
+            // 
+            this.tsmiQuery.Image = ((System.Drawing.Image)(resources.GetObject("tsmiQuery.Image")));
+            this.tsmiQuery.Name = "tsmiQuery";
+            this.tsmiQuery.Size = new System.Drawing.Size(178, 22);
+            this.tsmiQuery.Text = "成绩快速查询(&S)";
+            // 
+            // tsmi_AttendanceQuery
+            // 
+            this.tsmi_AttendanceQuery.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmi_Card,
+            this.toolStripSeparator7,
+            this.tsmi_AQuery});
+            this.tsmi_AttendanceQuery.Name = "tsmi_AttendanceQuery";
+            this.tsmi_AttendanceQuery.Size = new System.Drawing.Size(84, 21);
+            this.tsmi_AttendanceQuery.Text = "考勤管理(&A)";
+            // 
+            // tsmi_Card
+            // 
+            this.tsmi_Card.Image = ((System.Drawing.Image)(resources.GetObject("tsmi_Card.Image")));
+            this.tsmi_Card.Name = "tsmi_Card";
+            this.tsmi_Card.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_Card.Text = "考勤打卡(&R)";
+            // 
+            // toolStripSeparator7
+            // 
+            this.toolStripSeparator7.Name = "toolStripSeparator7";
+            this.toolStripSeparator7.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tsmi_AQuery
+            // 
+            this.tsmi_AQuery.Image = ((System.Drawing.Image)(resources.GetObject("tsmi_AQuery.Image")));
+            this.tsmi_AQuery.Name = "tsmi_AQuery";
+            this.tsmi_AQuery.Size = new System.Drawing.Size(180, 22);
+            this.tsmi_AQuery.Text = "考勤查询";
+            // 
+            // 帮助HToolStripMenuItem
+            // 
+            this.帮助HToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmi_linkxkt,
+            this.txmi_update,
+            this.toolStripSeparator8,
+            this.tsmi_about});
+            this.帮助HToolStripMenuItem.Name = "帮助HToolStripMenuItem";
+            this.帮助HToolStripMenuItem.Size = new System.Drawing.Size(61, 21);
+            this.帮助HToolStripMenuItem.Text = "帮助(&H)";
+            // 
+            // tsmi_linkxkt
+            // 
+            this.tsmi_linkxkt.Image = ((System.Drawing.Image)(resources.GetObject("tsmi_linkxkt.Image")));
+            this.tsmi_linkxkt.Name = "tsmi_linkxkt";
+            this.tsmi_linkxkt.Size = new System.Drawing.Size(141, 22);
+            this.tsmi_linkxkt.Text = "访问官网(&X)";
+            // 
+            // txmi_update
+            // 
+            this.txmi_update.Image = ((System.Drawing.Image)(resources.GetObject("txmi_update.Image")));
+            this.txmi_update.Name = "txmi_update";
+            this.txmi_update.Size = new System.Drawing.Size(141, 22);
+            this.txmi_update.Text = "系统升级(&U)";
+            // 
+            // toolStripSeparator8
+            // 
+            this.toolStripSeparator8.Name = "toolStripSeparator8";
+            this.toolStripSeparator8.Size = new System.Drawing.Size(138, 6);
+            // 
+            // tsmi_about
+            // 
+            this.tsmi_about.Image = ((System.Drawing.Image)(resources.GetObject("tsmi_about.Image")));
+            this.tsmi_about.Name = "tsmi_about";
+            this.tsmi_about.Size = new System.Drawing.Size(141, 22);
+            this.tsmi_about.Text = "关于我们(&A)";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblVersion,
+            this.toolStripStatusLabel1,
+            this.lblCurrentUser});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 707);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1264, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(85, 17);
+            this.lblVersion.Text = " 版本号：V2.0";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(100, 17);
+            this.toolStripStatusLabel1.Text = " [当前用登录户：";
+            // 
+            // lblCurrentUser
+            // 
+            this.lblCurrentUser.Name = "lblCurrentUser";
+            this.lblCurrentUser.Size = new System.Drawing.Size(48, 17);
+            this.lblCurrentUser.Text = "王晓军]";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmMain";
+            this.ClientSize = new System.Drawing.Size(1264, 729);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "[学员信息管理系统]--最适合初学者学习的实践项目";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 系统ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tmiModifyPwd;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem tmiClose;
+        private System.Windows.Forms.ToolStripMenuItem 学员管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAddStudent;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_Import;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiManageStudent;
+        private System.Windows.Forms.ToolStripMenuItem 成绩管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiQueryAndAnalysis;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiQuery;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_AttendanceQuery;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_Card;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator7;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_AQuery;
+        private System.Windows.Forms.ToolStripMenuItem 帮助HToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_linkxkt;
+        private System.Windows.Forms.ToolStripMenuItem txmi_update;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator8;
+        private System.Windows.Forms.ToolStripMenuItem tsmi_about;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblVersion;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblCurrentUser;
     }
 }
--- SMDB/SMDB/FrmUserLogin.Designer.cs
+++ SMDB/SMDB/FrmUserLogin.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUserLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txLoginPwd = new System.Windows.Forms.TextBox();
+            this.txtLoginId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(8, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(240, 160);
+            this.panel1.TabIndex = 0;
+            // 
+            // txLoginPwd
+            // 
+            this.txLoginPwd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txLoginPwd.Location = new System.Drawing.Point(347, 82);
+            this.txLoginPwd.Name = "txLoginPwd";
+            this.txLoginPwd.PasswordChar = '*';
+            this.txLoginPwd.Size = new System.Drawing.Size(120, 21);
+            this.txLoginPwd.TabIndex = 5;
+            this.txLoginPwd.Text = "195814";
+            // 
+            // txtLoginId
+            // 
+            this.txtLoginId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLoginId.Location = new System.Drawing.Point(347, 36);
+            this.txtLoginId.Name = "txtLoginId";
+            this.txtLoginId.Size = new System.Drawing.Size(120, 21);
+            this.txtLoginId.TabIndex = 2;
+            this.txtLoginId.Text = "龚均强";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(264, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "登录密码：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(264, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "登录账号：";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.Location = new System.Drawing.Point(313, 123);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(68, 27);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "  登 录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogout.Image = ((System.Drawing.Image)(resources.GetObject("btnLogout.Image")));
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(399, 123);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(68, 27);
+            this.btnLogout.TabIndex = 6;
+            this.btnLogout.Text = "  退 出";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // FrmUserLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmUserLogin";
+            this.ClientSize = new System.Drawing.Size(481, 178);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txLoginPwd);
+            this.Controls.Add(this.txtLoginId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmUserLogin";
+            this.Text = "学员管理系统--[用户登录]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txLoginPwd;
+        private System.Windows.Forms.TextBox txtLoginId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLogout;
     }
 }
